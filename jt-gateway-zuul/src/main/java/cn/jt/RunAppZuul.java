a176acc4f6564583b7160fc31ebcb758b742b5ae
csuftlgq <csuftligongqiang@163.com>
2018-09-12 15:00:29 +0800

    jt

---
+++ jt-gateway-zuul/src/main/java/cn/jt/RunAppZuul.java
@@ -0,0 +1,16 @@
+package cn.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringCloudApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class RunAppZuul {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppZuul.class, args);
+	}
+}
